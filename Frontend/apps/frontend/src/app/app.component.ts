0d41f1d9a367e05f957db712c6b7603e5078cd40
Najib Belhadj <najibbelhadj11@gmail.com>
2021-11-08 02:46:37 +0100

    NX ESLint Enabling Live Coding Linting

--- Frontend/apps/frontend/src/app/app.component.ts
+++ Frontend/apps/frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'Frontend-root',
+  selector: 'ngshop-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
