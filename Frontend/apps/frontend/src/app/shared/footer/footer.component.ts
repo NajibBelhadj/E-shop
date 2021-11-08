444436dd2a544cba239fc3d55cadcb2628938fff
Najib Belhadj <najibbelhadj11@gmail.com>
2021-11-08 02:28:21 +0100

    Naming Component Selector Rules with ESLint

--- Frontend/apps/frontend/src/app/shared/footer/footer.component.ts
+++ Frontend/apps/frontend/src/app/shared/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'Frontend-footer',
+  selector: 'ngshop-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
--- Frontend/apps/frontend/src/app/shared/header/header.component.ts
+++ Frontend/apps/frontend/src/app/shared/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'Frontend-header',
+  selector: 'ngshop-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
