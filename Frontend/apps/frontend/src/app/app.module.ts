eaab6a46a793f6235b8c90eb1608607dca5bfdf0
Najib Belhadj <najibbelhadj11@gmail.com>
2021-11-08 02:16:04 +0100

    Creating Routes

--- Frontend/apps/frontend/src/app/app.module.ts
+++ Frontend/apps/frontend/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'products',
+    component: ProductListComponent
 
+  },
+]
 @NgModule({
   declarations: [AppComponent, HomePageComponent, ProductListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
