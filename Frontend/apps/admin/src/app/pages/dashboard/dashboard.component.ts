26c322dba9b4b5e41525fcd09090e13b5dd22f23
Najib Belhadj <najibbelhadj11@gmail.com>
2021-11-08 03:08:35 +0100

    NX VSCode Extension

---
+++ Frontend/apps/admin/src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-dashbaord',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
