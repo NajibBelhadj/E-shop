844cc2bc166cb439dabaf0e89c85ee5d215e537f
Najib Belhadj <najibbelhadj11@gmail.com>
2021-12-03 17:54:59 +0100

    Disable Backent Authentication For API's For Frontend Purpose

--- Backend/helpers/jwt.js
+++ Backend/helpers/jwt.js
@@ -8,11 +8,12 @@ function authJwt() {
         isRevoked: isRevoked
     }).unless({
         path: [
-            { url: /\/public\/uploads(.*)/, method: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/products(.*)/, method: ['GET', 'OPTIONS'] },
-            { url: /\/api\/v1\/categories(.*)/, method: ['GET', 'OPTIONS'] },
-            '/api/v1/users/login',
-            '/api/v1/users/register'
+            //{ url: /\/public\/uploads(.*)/, method: ['GET', 'OPTIONS'] },
+            // { url: /\/api\/v1\/products(.*)/, method: ['GET', 'OPTIONS'] },
+            // { url: /\/api\/v1\/categories(.*)/, method: ['GET', 'OPTIONS'] },
+            // '/api/v1/users/login',
+            // '/api/v1/users/register'
+            { url: /(.*)/ },
         ]
     })
 }
