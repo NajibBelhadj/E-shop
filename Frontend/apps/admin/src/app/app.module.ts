a430ecf45cdeb8069e911394ddafc40f0d65b22d
Najib Belhadj <najibbelhadj11@gmail.com>
2021-12-03 16:03:00 +0100

    Creating The Routes and Show Shell

--- Frontend/apps/admin/src/app/app.module.ts
+++ Frontend/apps/admin/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
+const routes: Routes = [
+    {
+        path: '',
+        component: ShellComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            }
+        ]
+    }
+];
 @NgModule({
     declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule,RouterModule.forRoot(routes, {initialNavigation: 'enabled'})],
     providers: [],
     bootstrap: [AppComponent]
 })
