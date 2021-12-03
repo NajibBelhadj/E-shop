e8b0e6f85688c9d121a98fe541c5480cacb75768
Najib Belhadj <najibbelhadj11@gmail.com>
2021-12-03 15:50:11 +0100

    Building the Main Shell

--- Frontend/apps/admin/src/app/app.module.ts
+++ Frontend/apps/admin/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
-    declarations: [AppComponent, DashboardComponent],
+    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent],
     imports: [BrowserModule],
     providers: [],
     bootstrap: [AppComponent]
