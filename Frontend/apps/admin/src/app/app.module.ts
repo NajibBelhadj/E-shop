58b52f8129f0f7ff43d75281cb4a7e1f4d128515
Najib Belhadj <najibbelhadj11@gmail.com>
2021-12-03 19:40:29 +0100

    Add Categories Form - Use PrimeNG Forms

--- Frontend/apps/admin/src/app/app.module.ts
+++ Frontend/apps/admin/src/app/app.module.ts
@@ -8,18 +8,22 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CategoriesService } from '@Frontend/products';
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
 
 import {CardModule} from 'primeng/card';
 import {ToolbarModule} from 'primeng/toolbar';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
-import { CategoriesService } from '@Frontend/products';
+import {InputTextModule} from 'primeng/inputtext';
+
 
 const UX_MODULE = [
     CardModule,
     ToolbarModule,
     ButtonModule,
-    TableModule
+    TableModule,
+    InputTextModule
 ]
 
 const routes: Routes = [
@@ -34,12 +38,16 @@ const routes: Routes = [
             {
                 path: 'categories',
                 component: CategoriesListComponent
+            },
+            {
+                path: 'categories/form',
+                component: CategoriesFormComponent
             }
         ]
     }
 ];
 @NgModule({
-    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
+    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent, CategoriesFormComponent],
     imports: [
         BrowserModule,
         HttpClientModule,
